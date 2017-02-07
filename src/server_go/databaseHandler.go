954a17ed70dbff8669911450d6738f00452d27e8
Victor Zhang <zxjzxj9@gmail.com>
2017-02-07 09:16:52 +0800

    [devel]
    1. Fix the database initdb() function
    2. Add environment variables for building

--- src/server_go/databaseHandler.go
+++ src/server_go/databaseHandler.go
@@ -17,12 +17,45 @@ func initdb() {
 	defer db.Close()
 
 	checkTableExists := "SELECT name FROM sqlite_master WHERE type='table' AND name='user';"
-	res, err := db.Exec(checkTableExists)
+	res := db.QueryRow(checkTableExists)
 
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(-1)
 	}
 
+    var value interface{}
+    err := res.Scan(&value)
 
+    switch err {
+        case sql.ErrNoRows:
+            fmt.Println("No Table user detected, start building table")
+        case nil:
+            fmt.Println("Building database requires dropping user table, continue (y/N) ?")
+            rep := "N"
+            fmt.Scanln(rep)
+            switch rep {
+                case 'y':
+                    dropTable := "DROP TABLE user;"
+                    res, err := db.Exec(dropTable)
+                    if err != nil {
+                        fmt.Println("Dropping table error, exiting...")
+                        os.Exit(-1)
+                    }
+                case 'N':
+                    os.Exit(0)
+            }
+        default:
+            fmt.Println("Unkown error, exiting")
+            os.Exit(-1)
+    }
+
+    res, err :=  db.Exec(" CREATE TABLE user  "
+                         "( uid INTEGER,      "
+                         "  passwd INTEGER,   "
+                         "  login_time TEXT,  "
+                         "  online INTEGER,   "
+                         "  last_check TEXT );"
+                         )
+    fmt.Println(res)
 }
